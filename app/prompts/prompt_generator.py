dfff8d963f23315026776ee2683db2a2bb83f29f
Snail <snail1704@gmail.com>
2024-05-30 21:55:28 +0400

    4.5/5 correct answer

--- app/prompts/prompt_generator.py
+++ app/prompts/prompt_generator.py
@@ -1,4 +1,7 @@
-"""Module stores `PromptGenerator` which generate prompt from pieces like `PREFIX`, `SUFFIX`, etc."""
+"""
+Module stores `PromptGenerator` which 
+generate prompt from pieces like `PREFIX`, `SUFFIX`, etc.
+"""
 
 from typing import (
     Optional,
@@ -16,6 +19,8 @@ from app.prompts.sql_expert_prompt import (
 
 from langchain.embeddings.base import Embeddings
 
+from langchain_core.messages.ai import AIMessage
+from langchain_core.messages.human import HumanMessage
 from langchain_core.example_selectors import BaseExampleSelector
 
 from langchain_core.prompts import (
@@ -115,7 +120,152 @@ class PromptGenerator:
 
         self.prompt = ChatPromptTemplate.from_messages([
             SystemMessagePromptTemplate(prompt=few_shots_prompt),
-            ("human", "{input}"),
+            # TODO: refactor this examples messages to another module
+            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            HumanMessage(
+                content="Print the names of the passengers who flew to " +
+                    " California by Airbus A320 aircraft", 
+                example=True
+            ),
+            AIMessage(
+                content="{\'tool\': \'__list_sql_database_tool\', \'tool_input\': {}}",
+                example=True
+            ),
+            HumanMessage(
+                content="I can help you execute the tool. Give me a second... " +
+                    "And so, I think I managed to call it, but I can't read " +
+                    "what's written there, only a smart AI can understand it." +
+                    "\nI checked, the result of the tool\n" +
+                    "It looks like a list of table names. " +
+                    "Maybe it will help to get an response. I think if you don't " +
+                    "have enough information, you can use this: \'__info_sql_database_tool\'" +
+                    "\n\nResult of tool: \n" +
+                    "\'company, pass_in_trip, passenger, trip\'", 
+                example=True
+            ),
+            AIMessage(
+                content="{\'tool\': \'__info_sql_database_tool\', \'tool_input\': " +
+                    "{\'list_of_tables\': \'pass_in_trip, passenger, trip\'}}",
+                example=True
+            ),
+            HumanMessage(
+                content="I can help you execute the tool. Give me a second... " +
+                    "And so, I think I managed to call it, but I can't read " +
+                    "what's written there, only a smart AI can understand it." +
+                    "\nI checked, the result of the tool\n" +
+                    "It looks like the structure of the database " +
+                    "tables(s). Maybe this will help you write the right query? " +
+                    "Please make sure you remember the names!" +
+                    "\n\nResult of tool: \n" +
+                    "'\nCREATE TABLE pass_in_trip (\n\tpass_in_trip_id BIGSERIAL " +
+                    "NOT NULL, \n\ttrip_id BIGINT, \n\tpassenger_id BIGINT, " +
+                    "\n\tplace VARCHAR(60), \n\tCONSTRAINT pass_in_trip_pkey " +
+                    "PRIMARY KEY (pass_in_trip_id), \n\tCONSTRAINT fk_passanger " +
+                    "FOREIGN KEY(passenger_id) REFERENCES passenger (passenger_id), " +
+                    "\n\tCONSTRAINT fk_trip FOREIGN KEY(trip_id) REFERENCES trip " +
+                    "(trip_id)\n)\n\n/*\n3 rows from pass_in_trip " +
+                    "table:\npass_in_trip_id\ttrip_id\tpassenger_id\tplace" +
+                    "\n1\t1\t1\tA1\n2\t1\t2\tA2\n3\t1\t3\tA3\n*/\n\n\nCREATE " +
+                    "TABLE passenger (\n\tpassenger_id BIGSERIAL NOT NULL, " +
+                    "\n\tpassenger_name VARCHAR(60), \n\tCONSTRAINT passenger_pkey " +
+                    "PRIMARY KEY (passenger_id)\n)\n\n/*\n3 rows from passenger " +
+                    "table:\npassenger_id\tpassenger_name\n1\tJohn\n2\tJames\n3" +
+                    "\tPoul\n*/\n\n\nCREATE TABLE trip (\n\ttrip_id BIGSERIAL NOT " +
+                    "NULL, \n\tcompany_id BIGINT, \n\tplane VARCHAR(60), \n\ttown_from " +
+                    "VARCHAR(60), \n\ttown_to VARCHAR(60), \n\ttime_out TIMESTAMP " +
+                    "WITHOUT TIME ZONE, \n\ttime_in TIMESTAMP WITHOUT TIME ZONE, " +
+                    "\n\tCONSTRAINT trip_pkey PRIMARY KEY (trip_id), \n\tCONSTRAINT " +
+                    "fk_company FOREIGN KEY(company_id) REFERENCES company " +
+                    "(company_id)\n)\n\n/*\n3 rows from trip table:\ntrip_id" +
+                    "\tcompany_id\tplane\ttown_from\ttown_to\ttime_out\ttime_in" +
+                    "\n1\t1\tAirbus A320\tWashington\tCalifornia\t2021-02-14 " +
+                    "00:00:00\t2021-02-15 00:00:00\n2\t2\tAirbus A321-100\tTokio" +
+                    "\tParis\t2022-05-04 00:00:00\t2022-05-05 00:00:00\n3\t3\tAirbus " +
+                    "A319\tGongkong\tRiga\t2023-12-30 00:00:00\t2023-12-31 00:00:00\n*/'", 
+                example=True
+            ),
+            AIMessage(
+                content="{\'tool\': \'__query_sql_checker_tool\', \'tool_input\': " +
+                "{\'query_to_check\': \'SELECT passenger_name FROM passenger " +
+                "Pa LEFT JOIN pass_in_trip PaTr ON Pa.passenger_id = " +
+                "PaTr.passenger_id LEFT JOIN trip Tr ON PaTr.trip_id = " +
+                "Tr.trip_id WHERE town_to = \'California\' AND plane = " +
+                "\'Airbus A320\';\'} }",
+                example=True
+            ),
+            HumanMessage(
+                content="I can help you execute the tool. Give me a second... " +
+                    "And so, I think I managed to call it, but I can't read " +
+                    "what's written there, only a smart AI can understand it." +
+                    "\nI checked, the result of the tool\n" +
+                    "It looks like an SQL query! I think it's the " +
+                    "right one. Try to execute this query." +
+                    "\n\nResult of tool: \n" +
+                    "SELECT passenger_name FROM passenger Pa LEFT " +
+                    "JOIN pass_in_trip PaTr ON Pa.passenger_id = PaTr.passenger_id " +
+                    "LEFT JOIN trip Tr ON PaTr.trip_id = Tr.trip_id WHERE town_to " +
+                    "= \'California\' AND plane = \'Airbus A320\';", 
+                example=True
+            ),
+            AIMessage(
+                content="{\'tool\': \'__query_sql_checker_tool\', \'tool_input\': " +
+                "{\'query_to_check\': \'SELECT passenger_name FROM passenger " +
+                "Pa LEFT JOIN pass_in_trip PaTr ON Pa.passenger_id = " +
+                "PaTr.passenger_id LEFT JOIN trip Tr ON PaTr.trip_id = " +
+                "Tr.trip_id WHERE town_to = \'California\' AND plane = " +
+                "\'Airbus A320\';\'} }",
+                example=True
+            ),
+            HumanMessage(
+                content="I can help you execute the tool. Give me a second... " +
+                    "And so, I think I managed to call it, but I can't read " +
+                    "what's written there, only a smart AI can understand it." +
+                    "\nI checked, the result of the tool\n" +
+                    "It looks like an SQL query! I think it's the " +
+                    "right one. Try to execute this query." +
+                    "\n\nResult of tool: \n" +
+                    "SELECT passenger_name FROM passenger Pa LEFT JOIN " +
+                    "pass_in_trip PaTr ON Pa.passenger_id = " +
+                    "PaTr.passenger_id LEFT JOIN trip Tr ON PaTr.trip_id " +
+                    "= Tr.trip_id WHERE town_to = \'California\' AND " +
+                    "plane = \'Airbus A320\';", 
+                example=True
+            ),
+            AIMessage(
+                content="{\'tool\': \'__query_sql_database_tool\', \'tool_input\': " +
+                "{\'sql_query\': \'SELECT passenger_name FROM passenger Pa " +
+                "LEFT JOIN pass_in_trip PaTr ON Pa.passenger_id = PaTr.passenger_id " +
+                "LEFT JOIN trip Tr ON PaTr.trip_id = Tr.trip_id WHERE town_to = " +
+                "\'California\' AND plane = \'Airbus A320\';\'} }",
+                example=True
+            ),
+            HumanMessage(
+                content="I can help you execute the tool. Give me a second... " +
+                    "And so, I think I managed to call it, but I can't read " +
+                    "what's written there, only a smart AI can understand it." +
+                    "\nI checked, the result of the tool\n" +
+                    "This is similar to the database " +
+                    "response. Maybe there's something I need there. " +
+                    "I can't see it, please help me understand, if " +
+                    "there is an response to my question, please give it " +
+                    "to me, use it for this: \'__conversational_response\'" +
+                    "\n\nResult of tool: \n" +
+                    "[('John',), ('James',), ('Poul',), ('Christofer',), " +
+                    "('Superman',), ('Donald',), ('Douglas',), ('Dwight',), " +
+                    "('Earl',), ('Edgar',), ('Edmund',), ('Edwin',), ('Elliot',), " +
+                    "('Eric',), ('Ernest',)]", 
+                example=True
+            ),
+            AIMessage(
+                content="{\'tool\': \'__conversational_response\', \'tool_input\': " +
+                "{\'response\': \'These passengers are flying to California on " +
+                "an Airbus A320: 'John', 'James', 'Poul', 'Christofer', " +
+                "'Superman', 'Donald', 'Douglas', 'Dwight', 'Earl', 'Edgar', " +
+                "'Edmund', 'Edwin', 'Elliot', 'Eric', 'Ernest'.\'} }",
+                example=True
+            ),
+            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            ("human", "Well, thanks! Help me with another question now. {input}"),
             MessagesPlaceholder(variable_name="agent_scratchpad")
         ])
 
